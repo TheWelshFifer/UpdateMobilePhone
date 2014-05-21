ec56b353b3ddae4e4d49cb8eda364a8717a82ca9
TheWelshFifer <james@thewelshfifer.co.uk>
2014-05-21 21:34:53 +0100

    Initial upload

---
+++ UpdateMobilePhone.ps1
@@ -0,0 +1,6 @@
+﻿$user = get-aduser -filter * -properties * | where {$_.Mobilephone -ne $null -and $_.mobilephone.startswith("0")}
+
+foreach($u in $user)
+{
+    set-aduser $u -mobilephone("+44"+$u.mobilephone.trimstart("0"))
+}
